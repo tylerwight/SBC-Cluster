8a7fd2e929f2f12364ef340017516754d96e09b1
Tyler <tylermwight@gmail.com>
2018-12-01 16:42:10 -0700

    Fixed workaround

--- settingspatch.sh
+++ settingspatch.sh
@@ -1,4 +1,4 @@
 ## running into bug  https://github.com/kubernetes/kubeadm/issues/413
 #this is workaround to run during kubeadm init install
 
-watch -n 1.0 "sed -i 's/initialDelaySeconds: [0-9]\+/initialDelaySeconds: 180/' /etc/kubernetes/manifests/kube-apiserver.yaml"
+sudo watch -n 0.2 "sed -i 's/initialDelaySeconds: [0-9]\+/initialDelaySeconds: 800/' /etc/kubernetes/manifests/kube-apiserver.yaml"
